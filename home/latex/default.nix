9c3472f175c1850de32634d5dab6959059ff6444
Hervy Qurrotul Ainur Rozi <hervyqa@proton.me>
2024-01-02 13:51:46 +0700

    home: latex pkgs

---
+++ home/latex/default.nix
@@ -0,0 +1,33 @@
+{
+  config,
+  pkgs,
+  ...
+}: let
+  name = "hervyqa";
+in {
+  home-manager = {
+    users.${name} = {
+      home = {
+        packages = with pkgs;
+        with libsForQt5; [
+        (texlive.combine { inherit (texlive)
+          amsmath
+          capt-of
+          dvipng
+          dvisvgm
+          framed
+          hyperref
+          ifmtarg
+          paralist
+          scheme-basic
+          titlesec
+          ulem
+          wrapfig
+          xifthen;
+        })
+        texstudio
+        ];
+      };
+    };
+  };
+}
