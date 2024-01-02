6ff242c500809a6a4faabb3fa09c9f14cb7e63a5
Hervy Qurrotul Ainur Rozi <hervyqa@proton.me>
2024-01-02 10:46:18 +0700

    home: wofi config

---
+++ home/wofi/default.nix
@@ -0,0 +1,22 @@
+{
+  config,
+  pkgs,
+  ...
+}: let
+  name = "hervyqa";
+in {
+  home-manager = {
+    users.${name} = {
+      programs = {
+        wofi = {
+          enable = true;
+          settings = {
+            location = "bottom-right";
+            allow_markup = true;
+            width = 250;
+          };
+        };
+      };
+    };
+  };
+}
