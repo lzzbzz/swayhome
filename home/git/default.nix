727e1b841172285572c19dd31c361124087d1abb
Hervy Qurrotul Ainur Rozi <hervyqa@proton.me>
2024-01-02 10:39:58 +0700

    home: git config

---
+++ home/git/default.nix
@@ -0,0 +1,55 @@
+{
+  config,
+  pkgs,
+  ...
+}: let
+  name = "hervyqa";
+  email = "hervyqa@proton.me";
+  fullname = "Hervy Qurrotul Ainur Rozi";
+  # gpg --list-secret-keys --keyid-format=long
+  signingkey = "C10684E03E228DC0";
+in {
+  home-manager = {
+    users.${name} = {
+      programs = {
+        git = {
+          enable = true;
+          userEmail = "${email}";
+          userName = "${fullname}";
+          signing = {
+            key = "${signingkey}";
+          };
+          lfs = {
+            enable = true;
+          };
+          ignores = [
+            "*~"
+            "*.swp"
+          ];
+          extraConfig = {
+            core = {
+              whitespace = "trailing-space,space-before-tab"; 
+            };
+            url = {
+              "git@git.sr.ht:" = {
+                insteadOf = [
+                  "https://git.sr.ht/"
+                ];
+              };
+              "git@github.com:" = {
+                insteadOf = [
+                  "https://github.com/"
+                ];
+              };
+              "git@gitlab.com:" = {
+                insteadOf = [
+                  "https://gitlab.com/"
+                ];
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
