bd8b6fdd3ac41b9131e0a9927c0e6abd2b2ed358
Hervy Qurrotul Ainur Rozi <hervyqa@proton.me>
2024-01-02 11:16:37 +0700

    system: xdg portal settings

---
+++ system/xdg/default.nix
@@ -0,0 +1,23 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}: {
+  xdg = {
+    portal = {
+      enable = true;
+      wlr = {
+        enable = false;
+      };
+      configPackages = with pkgs; [
+        xdg-desktop-portal-gtk
+        xdg-desktop-portal-wlr
+      ];
+      extraPortals = with pkgs; [
+        xdg-desktop-portal-gtk
+        xdg-desktop-portal-wlr
+      ];
+    };
+  };
+}
