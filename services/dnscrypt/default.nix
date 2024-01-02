64fb8c7a67aebba80dfe7360361b4faa7b6eea20
Hervy Qurrotul Ainur Rozi <hervyqa@proton.me>
2024-01-02 10:54:13 +0700

    services: dnscrypt pkg

---
+++ services/dnscrypt/default.nix
@@ -0,0 +1,16 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}: {
+  services = {
+    dnscrypt-proxy2 = {
+      enable = true;
+      settings = {
+        ipv6_servers = true;
+        require_dnssec = true;
+      };
+    };
+  };
+}
