485f4e77be5bf6be3d9bb68b1372fbd1472d5672
Hervy Qurrotul Ainur Rozi <hervyqa@proton.me>
2024-01-02 11:44:20 +0700

    services: import nix

---
+++ services/default.nix
@@ -0,0 +1,26 @@
+{...}: {
+  imports = [
+    ./clamav
+    ./clickhouse
+    ./dnscrypt
+    ./earlyoom
+    ./espanso
+    ./fstrim
+    ./grafana
+    ./hdapsd
+    ./jupyter
+    ./metabase
+    ./mongodb
+    ./mysql
+    ./openssh
+    ./pipewire
+    ./postgresql
+    ./power-profiles
+    ./printing
+    ./redis
+    ./syncthing
+    ./udev
+    ./udisks2
+    ./xserver
+  ];
+}
