0c5ee3ed343b8c79b5e7a57a301d34dfb9a18c87
Hervy Qurrotul Ainur Rozi <hervyqa@proton.me>
2024-01-02 11:00:06 +0700

    services: xserver for desktop

---
+++ services/xserver/default.nix
@@ -0,0 +1,49 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}: let
+  name = "hervyqa";
+  layout = "us";
+in {
+  services = {
+    xserver = {
+      enable = true;
+      desktopManager = {
+        xterm = {
+          enable = false;
+        };
+      };
+      displayManager = {
+        sessionPackages = [ pkgs.sway ];
+        lightdm = {
+          enable = true;
+          greeter = {
+            enable = false;
+          };
+        };
+        autoLogin = {
+          enable = true;
+          user = "${name}";
+        };
+      };
+      excludePackages = with pkgs; [
+        xterm
+      ];
+      layout = "${layout}";
+      libinput = {
+        enable = true;
+      };
+      # default settings
+      videoDrivers = [
+        "amdgpu"
+        "radeon"
+        "nouveau"
+        "modesetting"
+        "fbdev"
+      ];
+      xkbVariant = "";
+    };
+  };
+}
