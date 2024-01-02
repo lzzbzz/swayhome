254a69c4ba19dd8ae524d6cbc6af8edfa1ce6ce3
Hervy Qurrotul Ainur Rozi <hervyqa@proton.me>
2024-01-02 10:56:44 +0700

    services: printing pkg

---
+++ services/printing/default.nix
@@ -0,0 +1,25 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}: {
+  services = {
+    printing = {
+      enable = true;
+      stateless = true;
+      webInterface = false;
+      drivers = with pkgs; [
+        epson-escpr
+        epson-escpr2
+        foomatic-db
+        gutenprint
+        hplip
+        splix
+      ];
+    };
+    system-config-printer = {
+      enable = true;
+    };
+  };
+}
