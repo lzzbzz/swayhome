720b2a2b572a71b7d31a5a83d25651d76148757a
Hervy Qurrotul Ainur Rozi <hervyqa@proton.me>
2024-01-02 11:12:51 +0700

    system: network settings

---
+++ system/network/default.nix
@@ -0,0 +1,36 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}: let
+  hostname = "nixos";
+in {
+  networking = {
+    hostName = "${hostname}";
+    networkmanager = {
+      enable = true;
+    };
+    wireless = {
+      # via wpa_supplicant.
+      enable = false;
+      iwd = {
+        enable = true;
+        settings = {
+          Network = {
+            EnableIPv6 = true;
+            RoutePriorityOffset = 300;
+          };
+          Settings = {
+            AutoConnect = true;
+          };
+        };
+      };
+    };
+    ## Configure network proxy if necessary.
+    # proxy = {
+    #   default = "http://user:password@proxy:port/";
+    #   noProxy = "127.0.0.1,localhost,internal.domain";
+    # };
+  };
+}
