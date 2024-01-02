bc1280f5ffd96d58c4c50b32a771913885019a49
Hervy Qurrotul Ainur Rozi <hervyqa@proton.me>
2024-01-02 11:46:30 +0700

    system: import nix

---
+++ system/default.nix
@@ -0,0 +1,22 @@
+{...}: {
+  imports = [
+    ./bluetooth
+    ./boot
+    ./documentation
+    ./environment
+    ./firewall
+    ./fonts
+    ./locale
+    ./network
+    ./nix
+    ./power
+    ./security
+    ./sound
+    ./system
+    ./systemd
+    ./timezone
+    ./users
+    ./xdg
+    ./zram
+  ];
+}
