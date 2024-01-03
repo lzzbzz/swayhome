14c9723acc80f66541cdd90746dac441a33c3734
Hervy Qurrotul Ainur Rozi <hervyqa@proton.me>
2024-01-03 15:04:51 +0700

    system: update environment for enable theme

--- system/environment/default.nix
+++ system/environment/default.nix
@@ -21,5 +21,16 @@ in {
       psql
       sqlite
     ];
+    sessionVariables = {
+      MOZ_ENABLE_WAYLAND = "1";
+      MOZ_USE_XINPUT2 = "1";
+      XDG_SESSION_TYPE = "wayland";
+      XDG_CURRENT_DESKTOP = "sway";
+      SDL_VIDEODRIVER = "wayland";
+      QT_QPA_PLATFORM="wayland";
+      QT_QPA_PLATFORMTHEME = "qt5ct";
+      QT_WAYLAND_DISABLE_WINDOWDECORATION="1";
+      QTWEBENGINE_CHROMIUM_FLAGS = "--force-dark-mode";
+    };
   };
 }
