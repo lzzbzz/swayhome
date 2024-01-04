28a632cb66b1988b8ada31e0dc26919eba18b048
Hervy Qurrotul Ainur Rozi <hervyqa@proton.me>
2024-01-04 17:51:46 +0700

    home: qutebrowser settings

--- home/qutebrowser/default.nix
+++ home/qutebrowser/default.nix
@@ -11,6 +11,28 @@ in {
         qutebrowser = {
           enable = true;
           settings = {
+            tabs.tabs_are_windows = true;
+            scrolling.smooth = true;
+            zoom.default = 90;
+            colors = {
+              hints = {
+                bg = "#2980b9";
+                fg = "#ffffff";
+              };
+              tabs.bar.bg = "#000000";
+            };
+            fonts = {
+              completion.entry = "8.0pt Monospace";
+              debug_console = "8.0pt Monospace";
+              default_family = [ "Monospace" "Liberation Mono" ];
+              downloads = "8.0pt Monospace";
+              hints = "8.0pt Monospace";
+              keyhint = "8.0pt Monospace";
+              messages.error = "8.0pt Monospace";
+              messages.info = "8.0pt Monospace";
+              messages.warning = "8.0pt Monospace";
+              statusbar = "8.0pt Monospace";
+            };
           };
         };
       };
