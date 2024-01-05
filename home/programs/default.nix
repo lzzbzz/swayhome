a1b7f79292bac652c0faa1e227b82451695be78c
Hervy Qurrotul Ainur Rozi <hervyqa@proton.me>
2024-01-05 08:12:53 +0700

    home: programs> remove session env because double var at home

--- home/programs/default.nix
+++ home/programs/default.nix
@@ -31,26 +31,4 @@ in {
     ./zathura
     ./zellij
   ];
-
-  home-manager = {
-    useGlobalPkgs = true;
-    users.${name} = {
-      home = {
-        homeDirectory = "/home/${name}";
-        stateVersion = "23.11";
-        sessionVariables = {
-          MOZ_ENABLE_WAYLAND = "1";
-          MOZ_USE_XINPUT2 = "1";
-          XDG_SESSION_TYPE = "wayland";
-          XDG_CURRENT_DESKTOP = "sway";
-          SDL_VIDEODRIVER = "wayland";
-          QT_QPA_PLATFORM="wayland";
-          QT_QPA_PLATFORMTHEME = "qt5ct";
-          QT_WAYLAND_DISABLE_WINDOWDECORATION="1";
-          QTWEBENGINE_CHROMIUM_FLAGS = "--force-dark-mode";
-          _JAVA_AWT_WM_NONREPARENTING=1;
-        };
-      };
-    };
-  };
 }
