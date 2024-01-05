dfcfa89394b1462e18cb0c70bb11688a177aa1a6
Hervy Qurrotul Ainur Rozi <hervyqa@proton.me>
2024-01-05 18:36:41 +0700

    home: sway> colors & border

--- home/wayland/sway.nix
+++ home/wayland/sway.nix
@@ -31,7 +31,7 @@ in {
               }];
               floating = {
                 modifier = "${mod}";
-                border = 0;
+                border = 3;
               };
               focus = {
                 forceWrapping = false;
@@ -74,6 +74,66 @@ in {
                 # print+save
                 "Print+${mod}" = ''exec ${grim}/bin/grim -g "$(${slurp}/bin/slurp -d)" | ${wl-clipboard}/bin/wl-copy -t image/png'';
                 "Print+Shift+${mod}" = ''exec ${grim}/bin/grim | ${wl-clipboard}/bin/wl-copy -t image/png'';
+              };
+              colors = let
+                bg-color = "#232629";
+                inactive-bg-color = "#31363b";
+                text-color = "#fcfcfc";
+                inactive-text-color = "#7f8c8d";
+                urgent-bg-color = "#3daee9";
+                indicator = "#4d4d4d";
+              in {
+                background = bg-color;
+                focused = {
+                  border = bg-color;
+                  background = bg-color;
+                  text = text-color;
+                  indicator = indicator;
+                  childBorder = bg-color;
+                };
+                focusedInactive = {
+                  border = inactive-bg-color;
+                  background = inactive-bg-color;
+                  text = inactive-text-color;
+                  indicator = indicator;
+                  childBorder = inactive-bg-color;
+                };
+                unfocused = {
+                  border = inactive-bg-color;
+                  background = inactive-bg-color;
+                  text = inactive-text-color;
+                  indicator = indicator;
+                  childBorder = inactive-bg-color;
+                };
+                urgent = {
+                  border = urgent-bg-color;
+                  background = urgent-bg-color;
+                  text = text-color;
+                  indicator = indicator;
+                  childBorder = urgent-bg-color;
+                };
+                placeholder = {
+                  border = urgent-bg-color;
+                  background = urgent-bg-color;
+                  text = text-color;
+                  indicator = indicator;
+                  childBorder = urgent-bg-color;
+                };
+              };
+              modes = {
+                resize = let
+                  left = "h";
+                  down = "j";
+                  up = "k";
+                  right = "l";
+                in {
+                  Escape = "mode default";
+                  Return = "mode default";
+                  "${down}" = "resize grow height 10 px or 10 ppt";
+                  "${left}" = "resize shrink width 10 px or 10 ppt";
+                  "${right}" = "resize grow width 10 px or 10 ppt";
+                  "${up}" = "resize shrink height 10 px or 10 ppt";
+                };
               };
             };
             swaynag = {
