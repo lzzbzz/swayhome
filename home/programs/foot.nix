c9c97647fdabd836d4dbd114be5316101978b011
Hervy Qurrotul Ainur Rozi <hervyqa@proton.me>
2024-01-06 20:01:12 +0700

    home: foot> new colors

--- home/programs/foot.nix
+++ home/programs/foot.nix
@@ -21,6 +21,24 @@ in {
             };
             colors = {
               alpha = 0.9;
+              background = "232629";
+              foreground = "eceff1";
+              regular0 = "232629";  # black
+              regular1 = "da4453";  # red
+              regular2 = "27ae60";  # green
+              regular3 = "fdbc4b";  # yellow
+              regular4 = "1e92ff";  # blue
+              regular5 = "f67400";  # magenta
+              regular6 = "3daee6";  # cyan
+              regular7 = "ffffff";  # white
+              bright0 = "b0bec5";   # bright black
+              bright1 = "ff8a80";   # bright red
+              bright2 = "b9f6ca";   # bright green
+              bright3 = "ffe57f";   # bright yellow
+              bright4 = "80d8ff";   # bright blue
+              bright5 = "ff80ab";   # bright magenta
+              bright6 = "a7fdeb";   # bright cyan
+              bright7 = "ffffff";   # bright white
             };
           };
         };
