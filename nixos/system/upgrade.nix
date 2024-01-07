89caf1f21aec5838fc0603d44e6b924e8dcc9200
Hervy Qurrotul Ainur Rozi <hervyqa@proton.me>
2024-01-07 09:17:38 +0700

    nixos: system> rename to upgrade.nix

--- nixos/system/upgrade.nix
+++ nixos/system/upgrade.nix
@@ -3,14 +3,11 @@
   pkgs,
   lib,
   ...
-}: let
-  ver = "23.11";
-in {
+}: {
   system = {
     autoUpgrade = {
       enable = false;
       allowReboot = false;
     };
-    stateVersion = "${ver}";
   };
 }
