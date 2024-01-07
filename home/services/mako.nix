9f067dd5f57181bb2d6c390d8363215043287e96
Hervy Qurrotul Ainur Rozi <hervyqa@proton.me>
2024-01-07 11:21:37 +0700

    home: mako> colors & settings

--- home/services/mako.nix
+++ home/services/mako.nix
@@ -2,14 +2,30 @@
   config,
   pkgs,
   ...
-}: let
+}:
+let
   name = "hervyqa";
+  # colors
+  bg-color = "#3daee9";
+  inactive-bg-color = "#232629";
+  text-color = "#fcfcfc";
+  inactive-text-color = "#7f8c8d";
+  urgent-bg-color = "#3daee9";
+  indicator = "#3daee9";
 in {
   home-manager = {
     users.${name} = {
       services = {
         mako = {
           enable = true;
+          defaultTimeout = 5000; # 5s
+          font = "monospace 8";
+          margin = "30";
+          padding = "5";
+          borderSize = 5;
+          borderColor = bg-color;
+          backgroundColor = inactive-bg-color;
+          textColor = text-color;
         };
       };
     };
