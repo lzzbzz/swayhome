31efcf50a50ddf933b97908b4957c76451b6c219
Hervy Qurrotul Ainur Rozi <hervyqa@proton.me>
2024-01-07 13:05:46 +0700

    home: cava> settings & colors

--- home/programs/cava.nix
+++ home/programs/cava.nix
@@ -11,6 +11,18 @@ in {
         cava = {
           enable = true;
           settings = {
+            general = {
+              framerate = 60;
+              bars = 0;
+              bar_width = 1;
+              bar_spacing = 1;
+            };
+            smoothing = {
+              noise_reduction = 88;
+            };
+            color = {
+              foreground = "'#3daee9'";
+            };
           };
         };
       };
