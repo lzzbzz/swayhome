b3e4895efd482211f185ceb303bf8bd7682feaec
Hervy Qurrotul Ainur Rozi <hervyqa@proton.me>
2024-01-08 20:27:25 +0700

    home: qutebrowser> color var

--- home/programs/qutebrowser.nix
+++ home/programs/qutebrowser.nix
@@ -4,6 +4,33 @@
   ...
 }: let
   name = "hervyqa";
+
+  # colors
+  abyss-blue = "#2980b9";
+  alternate-gray = "#bdc3c7";
+  beware-orange = "#f67400";
+  cadmium-orange = "#ed872d";
+  cardboard-gray = "#eff0f1";
+  charcoal-gray = "#31363b";
+  citrus-yellow = "#fcd116";
+  coastal-fog = "#7f8c8d";
+  hover-blue = "#93cee9";
+  hyper-blue = "#3daee6";
+  icon-blue = "#1d99f3";
+  icon-gray = "#4d4d4d";
+  icon-red = "#da4453";
+  icon-yellow = "#fdbc4b";
+  lazy-gray = "#afb0b3";
+  lightsaber-blue = "#15f2fd";
+  noble-fir = "#27ae60";
+  paper-white = "#fcfcfc";
+  pimpinella= "#e74c3c";
+  plasma-blue = "#3daee9";
+  red-pink = "#fa2a55";
+  shade-black = "#232629";
+  spring-grass ="#00ff7c";
+  sunbeam-yellow = "#c9ce3b";
+
 in {
   home-manager = {
     users.${name} = {
@@ -16,29 +43,29 @@ in {
             zoom.default = 90;
             colors = {
               hints = {
-                bg = "#232629";
-                fg = "#fcfcfc";
+                bg = shade-black;
+                fg = paper-white;
               };
               tabs = {
                 bar = {
-                  bg = "#232629";
+                  bg = shade-black;
                 };
                 even = {
-                  bg = "#232629";
-                  fg = "#fcfcfc";
+                  bg = shade-black;
+                  fg = paper-white;
                 };
                 odd = {
-                  bg = "#232629";
-                  fg = "#fcfcfc";
+                  bg = shade-black;
+                  fg = paper-white;
                 };
                 selected = {
                   even = {
-                    bg = "#3daee9";
-                    fg = "#fcfcfc";
+                    bg = charcoal-gray;
+                    fg = paper-white;
                   };
                   odd = {
-                    bg = "#3daee9";
-                    fg = "#fcfcfc";
+                    bg = charcoal-gray;
+                    fg = paper-white;
                   };
                 };
               };
