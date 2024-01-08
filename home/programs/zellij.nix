96ae64765a5e471d412b5559a865f97ec8e56fc8
Hervy Qurrotul Ainur Rozi <hervyqa@proton.me>
2024-01-09 04:01:14 +0700

    home: zellij> color var

--- home/programs/zellij.nix
+++ home/programs/zellij.nix
@@ -4,6 +4,35 @@
   ...
 }: let
   name = "hervyqa";
+
+  # colors
+  abyss-blue = "#2980b9";
+  alternate-gray = "#bdc3c7";
+  beware-orange = "#f67400";
+  cadmium-orange = "#ed872d";
+  cardboard-gray = "#eff0f1";
+  charcoal-gray = "#31363b";
+  citrus-yellow = "#fcd116";
+  coastal-fog = "#7f8c8d";
+  hot-purple = "#cb00f5";
+  hover-blue = "#93cee9";
+  hyper-blue = "#3daee6";
+  icon-blue = "#1d99f3";
+  icon-gray = "#4d4d4d";
+  icon-red = "#da4453";
+  icon-yellow = "#fdbc4b";
+  lazy-gray = "#afb0b3";
+  lightsaber-blue = "#15f2fd";
+  noble-fir = "#27ae60";
+  paper-white = "#fcfcfc";
+  pimpinella= "#e74c3c";
+  pinkish-purple = "#d648d7";
+  plasma-blue = "#3daee9";
+  red-pink = "#fa2a55";
+  shade-black = "#232629";
+  spring-grass ="#00ff7c";
+  sunbeam-yellow = "#c9ce3b";
+
 in {
   home-manager = {
     users.${name} = {
@@ -16,6 +45,28 @@ in {
             default_shell = "fish";
             pane_frames = false;
             simplified_ui = true;
+            ui = {
+              pane_frames = {
+                hide_session_name = true;
+                rounded_corners = true;
+              };
+            };
+            theme = "default";
+            themes = {
+              default = {
+                fg = paper-white;
+                bg = shade-black;
+                black = charcoal-gray;
+                red = icon-red;
+                green = noble-fir;
+                yellow = icon-yellow;
+                blue = plasma-blue;
+                magenta = hot-purple;
+                cyan = hyper-blue;
+                white = paper-white;
+                orange = beware-orange;
+              };
+            };
           };
         };
       };
