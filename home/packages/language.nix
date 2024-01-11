ed74615a326acc602bede47add2895d667860f40
Hervy Qurrotul Ainur Rozi <hervyqa@proton.me>
2024-01-12 06:22:18 +0700

    home: languages> new languages server

--- home/packages/language.nix
+++ home/packages/language.nix
@@ -8,7 +8,9 @@ in {
     users.${name} = {
       home = {
         packages = with pkgs;
-        with pkgs.nodePackages; [
+        with pkgs.elmPackages;
+        with pkgs.nodePackages;
+        with pkgs.solargraph; [
           # haskell-language-server # haskell # too much storage
           # sourcekit-lsp # swift & objective-c # too much storage
           ansible-language-server # ansible
@@ -16,11 +18,16 @@ in {
           clang # clangd
           clojure-lsp # clojure
           cmake-language-server # cmake
+          crystalline # cairo
           cuelsp # cue
           dhall-lsp-server # dhall
           dot-language-server # dot
           elixir-ls # elixir
+          elm-language-server # elm
           elvish # elvish
+          erlang-ls # erlang
+          fortls # fortran
+          fsautocomplete # fsharp
           gleam # gleam
           gopls # golang
           intelephense # php
@@ -34,20 +41,26 @@ in {
           nil # nix
           nimlsp # nim
           nls # nickel
+          openscad-lsp # openscad
           perlnavigator # perl
+          protobuf # protobuf
           purescript-language-server # purescript
           racket # racket
           regols # rego
           ruff-lsp # python
           rust-analyzer # rust
           slint-lsp # slint
+          solargraph # ruby
           solc # solidity
           svelte-language-server # svelte
+          swiProlog # prolog
           taplo # toml
           terraform-ls # terraform
           texlab # latex
           typescript-language-server # typescript
           vala-language-server # vala
+          vhdl-ls # vhdl
+          vls # vue
           yaml-language-server # yaml
           zls # zig
         ];
