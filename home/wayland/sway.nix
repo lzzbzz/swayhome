37fc6d93dfef9b8d5578d22d8aca4d6e892b4f33
Hervy Qurrotul Ainur Rozi <hervyqa@proton.me>
2024-01-13 11:42:19 +0700

    home: sway> layout and workspace

--- home/wayland/sway.nix
+++ home/wayland/sway.nix
@@ -9,6 +9,11 @@ let
   name = "hervyqa";
   modifier = "Mod4";
 
+  left = "h";
+  down = "j";
+  up = "k";
+  right = "l";
+
   # colors
   abyss-blue = "#2980b9";
   alternate-gray = "#bdc3c7";
@@ -45,10 +50,6 @@ in {
           sway = {
             enable = true;
             config = {
-              left = "h";
-              down = "j";
-              up = "k";
-              right = "l";
               gaps = {
                 inner = 10;
               };
@@ -91,6 +92,43 @@ in {
               keybindings = mkOptionDefault {
                 # menu
                 "${modifier}+d" = "exec ${dmenu}/bin/dmenu_path | ${dmenu}/bin/dmenu -fn 'Monospace:size=10' -nf '${paper-white}' -nb '${shade-black}' -sf '${paper-white}' -sb '${plasma-blue}' | ${findutils}/bin/xargs swaymsg exec --";
+
+                "${modifier}+1" = "workspace number 1";
+                "${modifier}+2" = "workspace number 2";
+                "${modifier}+3" = "workspace number 3";
+                "${modifier}+4" = "workspace number 4";
+                "${modifier}+5" = "workspace number 5";
+                "${modifier}+6" = "workspace number 6";
+                "${modifier}+7" = "workspace number 7";
+                "${modifier}+8" = "workspace number 8";
+                "${modifier}+9" = "workspace number 9";
+                "${modifier}+0" = "workspace number 10";
+
+                "${modifier}+Shift+1" = "move container to workspace number 1";
+                "${modifier}+Shift+2" = "move container to workspace number 2";
+                "${modifier}+Shift+3" = "move container to workspace number 3";
+                "${modifier}+Shift+4" = "move container to workspace number 4";
+                "${modifier}+Shift+5" = "move container to workspace number 5";
+                "${modifier}+Shift+6" = "move container to workspace number 6";
+                "${modifier}+Shift+7" = "move container to workspace number 7";
+                "${modifier}+Shift+8" = "move container to workspace number 8";
+                "${modifier}+Shift+9" = "move container to workspace number 9";
+                "${modifier}+Shift+0" = "move container to workspace number 10";
+
+                "${modifier}+${left}" = "focus left";
+                "${modifier}+${down}" = "focus down";
+                "${modifier}+${up}" = "focus up";
+                "${modifier}+${right}" = "focus right";
+
+                "${modifier}+Ctrl+${left}" = "move workspace to output left";
+                "${modifier}+Ctrl+${down}" = "move workspace to output down";
+                "${modifier}+Ctrl+${up}" = "move workspace to output up";
+                "${modifier}+Ctrl+${right}" = "move workspace to output right";
+
+                "${modifier}+Shift+${left}" = "move left";
+                "${modifier}+Shift+${down}" = "move down";
+                "${modifier}+Shift+${up}" = "move up";
+                "${modifier}+Shift+${right}" = "move right";
 
                 # audio control
                 "XF86AudioRaiseVolume" = "exec ${swayosd}/bin/swayosd --output-volume 2";
