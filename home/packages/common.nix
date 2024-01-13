6dcd8764dae3c636dcfb02b0934deb66953530ab
Hervy Qurrotul Ainur Rozi <hervyqa@proton.me>
2024-01-13 08:15:53 +0700

    home: common> vhs pkgs

--- home/packages/common.nix
+++ home/packages/common.nix
@@ -45,6 +45,7 @@ in {
           translate-shell # translate via google api
           upterm # remote console
           ventoy # cerate usb bootable
+          vhs # generating terminal gifs with code
           vulkan-tools # khronos official vulkan tools and utilities
           w3m # image viewer
           wayland-utils # wayland utilities (wayland-info)
