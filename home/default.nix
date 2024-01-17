6789c94584a44c59e66ebb0c8ed3c82cd1f607e6
Hervy Qurrotul Ainur Rozi <hervyqa@proton.me>
2024-01-18 04:39:24 +0700

    environment: sway session & wayland stuff

--- home/default.nix
+++ home/default.nix
@@ -19,14 +19,18 @@ in {
         homeDirectory = "/home/${name}";
         stateVersion = "${ver}";
         sessionVariables = {
+          # session.
+          XDG_CURRENT_DESKTOP = "sway";
+          XDG_SESSION_DESKTOP = "sway";
+          XDG_SESSION_TYPE = "wayland";
+
+          # wayland stuff.
           MOZ_ENABLE_WAYLAND = "1";
           MOZ_USE_XINPUT2 = "1";
-          XDG_SESSION_TYPE = "wayland";
-          XDG_CURRENT_DESKTOP = "sway";
-          SDL_VIDEODRIVER = "wayland";
-          QT_QPA_PLATFORM= "wayland";
+          QT_QPA_PLATFORM="wayland";
           QT_QPA_PLATFORMTHEME = "qt5ct";
           QT_WAYLAND_DISABLE_WINDOWDECORATION="1";
+          SDL_VIDEODRIVER = "wayland";
           _JAVA_AWT_WM_NONREPARENTING=1;
         };
       };
