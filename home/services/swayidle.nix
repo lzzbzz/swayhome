2fa0ca72531cf516890ade8c4b33310fac68d315
Hervy Qurrotul Ainur Rozi <hervyqa@proton.me>
2024-01-24 07:42:17 +0700

    home: swayidle> timeouts config

--- home/services/swayidle.nix
+++ home/services/swayidle.nix
@@ -13,6 +13,10 @@ in {
             { event = "before-sleep"; command = "${pkgs.swaylock}/bin/swaylock -fF"; }
             { event = "lock"; command = "lock"; }
           ];
+          timeouts = [
+            { timeout = 60; command = "${pkgs.swaylock}/bin/swaylock -fF"; }
+            { timeout = 90; command = "${pkgs.systemd}/bin/systemctl suspend"; }
+          ];
         };
       };
     };
