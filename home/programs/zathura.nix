c7e20da4ed0957d82895c0e4ece7249d7cb163c3
Hervy Qurrotul Ainur Rozi <hervyqa@proton.me>
2024-01-25 16:19:07 +0700

    home: zathura> config rc

--- home/programs/zathura.nix
+++ home/programs/zathura.nix
@@ -7,6 +7,11 @@ in {
         zathura = {
           enable = true;
           options = {
+            adjust-open = "best-fit";
+            font = "Monospace 8";
+            pages-per-row = 1;
+            scroll-page-aware = true;
+            scroll-step = 50;
           };
         };
       };
