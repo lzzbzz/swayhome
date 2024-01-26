2bbf47181e40ad41253f3f076087d4ea0eae8862
Hervy Qurrotul Ainur Rozi <hervyqa@proton.me>
2024-01-26 21:16:53 +0700

    home: ncmpcpp> keybindings

--- home/programs/ncmpcpp.nix
+++ home/programs/ncmpcpp.nix
@@ -6,8 +6,12 @@ in {
       programs = {
         ncmpcpp = {
           enable = true;
-          settings = {
-          };
+          bindings = [
+            { key = "j"; command = "scroll_down"; }
+            { key = "k"; command = "scroll_up"; }
+            { key = "J"; command = [ "select_item" "scroll_down" ]; }
+            { key = "K"; command = [ "select_item" "scroll_up" ]; }
+          ];
         };
       };
     };
