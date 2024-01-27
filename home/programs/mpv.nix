a12a879369b29bece22981562bebf94cb975aa25
Hervy Qurrotul Ainur Rozi <hervyqa@proton.me>
2024-01-27 22:48:27 +0700

    home: mpv> fullscreen yes

--- home/programs/mpv.nix
+++ home/programs/mpv.nix
@@ -8,7 +8,7 @@ in {
           enable = true;
           config = {
             force-window = "yes";
-            fullscreen = "no";
+            fullscreen = "yes";
             profile = "gpu-hq";
             ytdl-format = "bestvideo+bestaudio";
           };
