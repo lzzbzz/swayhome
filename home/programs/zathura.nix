96854df5c2a2184f4a37ca35d62c7983ec84f013
Hervy Qurrotul Ainur Rozi <hervyqa@proton.me>
2024-01-29 11:29:16 +0700

    home: zathura> color options

--- home/programs/zathura.nix
+++ home/programs/zathura.nix
@@ -1,5 +1,9 @@
 { ... }: let
   name = "hervyqa";
+  plasma-blue = "#3daee9";
+  paper-white = "#fcfcfc";
+  shade-black = "#232629";
+  charcoal-gray = "#31363b";
 in {
   home-manager = {
     users.${name} = {
@@ -7,11 +11,27 @@ in {
         zathura = {
           enable = true;
           options = {
-            adjust-open = "best-fit";
+            adjust-open = "width";
             font = "Monospace 8";
             pages-per-row = 1;
             scroll-page-aware = true;
             scroll-step = 50;
+            completion-bg = "${charcoal-gray}";
+            completion-fg = "${paper-white}";
+            completion-highlight-bg = "${plasma-blue}";
+            default-bg = "${shade-black}";
+            default-fg = "${paper-white}";
+            hightlight-color = "${plasma-blue}";
+            hightlight-fg = "${shade-black}";
+            inputbar-bg = "${shade-black}";
+            inputbar-fg = "${paper-white}";
+            notification-bg = "${plasma-blue}";
+            notification-fg = "${shade-black}";
+            recolor-darkcolor = "${paper-white}";
+            recolor-lightcolor = "${shade-black}";
+            statusbar-bg = "${shade-black}";
+            statusbar-fg = "${paper-white}";
+            window-title-basename = true;
           };
         };
       };
