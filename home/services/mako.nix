83f1e7480342f661809227c147266b48fe564f28
Hervy Qurrotul Ainur Rozi <hervyqa@proton.me>
2024-02-03 12:02:39 +0700

    home: mako> catppuccin: colors

--- home/services/mako.nix
+++ home/services/mako.nix
@@ -2,32 +2,9 @@
   name = "hervyqa";
 
   # colors
-  abyss-blue = "#2980b9";
-  alternate-gray = "#bdc3c7";
-  beware-orange = "#f67400";
-  cadmium-orange = "#ed872d";
-  cardboard-gray = "#eff0f1";
-  charcoal-gray = "#31363b";
-  citrus-yellow = "#fcd116";
-  coastal-fog = "#7f8c8d";
-  hot-purple = "#af69ef";
-  hover-blue = "#93cee9";
-  hyper-blue = "#3daee6";
-  icon-blue = "#1d99f3";
-  icon-gray = "#4d4d4d";
-  icon-red = "#da4453";
-  icon-yellow = "#fdbc4b";
-  lazy-gray = "#afb0b3";
-  lightsaber-blue = "#15f2fd";
-  noble-fir = "#27ae60";
-  paper-white = "#fcfcfc";
-  pimpinella= "#e74c3c";
-  pinkish-purple = "#d648d7";
-  plasma-blue = "#3daee9";
-  red-pink = "#fa2a55";
-  shade-black = "#232629";
-  spring-grass ="#32e17c";
-  sunbeam-yellow = "#c9ce3b";
+  foreground = "#C6D0F5"; # text
+  background = "#303446"; # base
+  bright7 = "#A5ADCE";    # subtext 0
 
 in {
   home-manager = {
@@ -40,9 +17,9 @@ in {
           margin = "30";
           padding = "5";
           borderSize = 3;
-          borderColor = plasma-blue;
-          backgroundColor = shade-black;
-          textColor = paper-white;
+          borderColor = bright7;
+          backgroundColor = background;
+          textColor = foreground;
         };
       };
     };
