5f37c832dc36c5694619f7e84e0340483cb79d34
Hervy Qurrotul Ainur Rozi <hervyqa@proton.me>
2024-02-03 12:00:59 +0700

    home: rofi> catppuccin: colors

--- home/programs/rofi.nix
+++ home/programs/rofi.nix
@@ -5,33 +5,6 @@
   let
   name = "hervyqa";
 
-  # colors
-  abyss-blue = "#2980b9";
-  alternate-gray = "#bdc3c7";
-  beware-orange = "#f67400";
-  cadmium-orange = "#ed872d";
-  cardboard-gray = "#eff0f1";
-  charcoal-gray = "#31363b";
-  citrus-yellow = "#fcd116";
-  coastal-fog = "#7f8c8d";
-  hot-purple = "#af69ef";
-  hover-blue = "#93cee9";
-  hyper-blue = "#3daee6";
-  icon-blue = "#1d99f3";
-  icon-gray = "#4d4d4d";
-  icon-red = "#da4453";
-  icon-yellow = "#fdbc4b";
-  lazy-gray = "#afb0b3";
-  lightsaber-blue = "#15f2fd";
-  noble-fir = "#27ae60";
-  paper-white = "#fcfcfc";
-  pimpinella= "#e74c3c";
-  pinkish-purple = "#d648d7";
-  plasma-blue = "#3daee9";
-  red-pink = "#fa2a55";
-  shade-black = "#232629";
-  spring-grass ="#32e17c";
-  sunbeam-yellow = "#c9ce3b";
 in {
   home-manager = {
     users.${name} = {
@@ -45,7 +18,7 @@ in {
             case-sensitive = false;
             display-drun = "Apps:";
             modi = [ "drun" "run" ];
-            show-icons = false;
+            show-icons = true;
           };
           pass = {
             enable = true;
@@ -59,9 +32,9 @@ in {
           theme = let mkLiteral = config.lib.formats.rasi.mkLiteral;
             in {
               "*" = {
-                bg = mkLiteral "${shade-black}";
-                fg = mkLiteral "${paper-white}";
-                ac = mkLiteral "${plasma-blue}";
+                bg = mkLiteral  "#303446FF";
+                fg = mkLiteral  "#D9E0EEFF";
+                ac = mkLiteral "#A5ADCEFF"; #sel
               };
 
               "#window" = {
