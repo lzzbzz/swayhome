f1c7e594a3f1d9e8bcfbf570db6c85088934f585
Hervy Qurrotul Ainur Rozi <hervyqa@proton.me>
2024-02-03 11:59:36 +0700

    home: foot> catppuccin: colors

--- home/programs/foot.nix
+++ home/programs/foot.nix
@@ -1,34 +1,6 @@
 { ... }: let
   name = "hervyqa";
 
-  # colors without hastag
-  abyss-blue = "2980b9";
-  alternate-gray = "bdc3c7";
-  beware-orange = "f67400";
-  cadmium-orange = "ed872d";
-  cardboard-gray = "eff0f1";
-  charcoal-gray = "31363b";
-  citrus-yellow = "fcd116";
-  coastal-fog = "7f8c8d";
-  hot-purple = "af69ef";
-  hover-blue = "93cee9";
-  hyper-blue = "3daee6";
-  icon-blue = "1d99f3";
-  icon-gray = "4d4d4d";
-  icon-red = "da4453";
-  icon-yellow = "fdbc4b";
-  lazy-gray = "afb0b3";
-  lightsaber-blue = "15f2fd";
-  noble-fir = "27ae60";
-  paper-white = "fcfcfc";
-  pimpinella= "e74c3c";
-  pinkish-purple = "d648d7";
-  plasma-blue = "3daee9";
-  red-pink = "fa2a55";
-  shade-black = "232629";
-  spring-grass ="32e17c";
-  sunbeam-yellow = "c9ce3b";
-
 in {
   home-manager = {
     users.${name} = {
@@ -44,26 +16,26 @@ in {
             mouse = {
               hide-when-typing = "yes";
             };
-            colors = {
+            colors = { # colorscheme: catppuccin-frappe
               alpha = 1.0;
-              background = shade-black;
-              foreground = paper-white;
-              regular0 = shade-black; # black
-              regular1 = icon-red; # red
-              regular2 = noble-fir; # green
-              regular3 = icon-yellow; # yellow
-              regular4 = plasma-blue; # blue
-              regular5 = hot-purple; # magenta
-              regular6 = hyper-blue; # cyan
-              regular7 = paper-white; # white
-              bright0 = coastal-fog;  # bright black
-              bright1 = red-pink; # bright red
-              bright2 = spring-grass; # bright green
-              bright3 = sunbeam-yellow; # bright yellow
-              bright4 = plasma-blue; # bright blue
-              bright5 = pinkish-purple; # bright magenta
-              bright6 = lightsaber-blue; # bright cyan
-              bright7 = paper-white; # bright white
+              foreground = "C6D0F5"; # text
+              background = "303446"; # base
+              regular0 = "51576D";   # surface 1
+              regular1 = "E78284";   # red
+              regular2 = "A6D189";   # green
+              regular3 = "E5C890";   # yellow
+              regular4 = "8CAAEE";   # blue
+              regular5 = "F4B8E4";   # pink
+              regular6 = "81C8BE";   # teal
+              regular7 = "B5BFE2";   # subtext 1
+              bright0 = "626880";    # surface 2
+              bright1 = "E78284";    # red
+              bright2 = "A6D189";    # green
+              bright3 = "E5C890";    # yellow
+              bright4 = "8CAAEE";    # blue
+              bright5 = "F4B8E4";    # pink
+              bright6 = "81C8BE";    # teal
+              bright7 = "A5ADCE";    # subtext 0
             };
           };
         };
