fede4de7e807b0d8a4e887f48810cb615995a77f
Hervy Qurrotul Ainur Rozi <hervyqa@proton.me>
2024-02-04 15:12:54 +0700

    home: qutebrowser> revert disable unused colors

--- home/programs/qutebrowser.nix
+++ home/programs/qutebrowser.nix
@@ -3,28 +3,28 @@
 
   # colors
   rosewater = "#F2D5CF";
-  #flamingo  = "#EEBEBE";
-  #pink      = "#F4B8E4";
-  #mauve     = "#CA9EE6";
+  flamingo  = "#EEBEBE";
+  pink      = "#F4B8E4";
+  mauve     = "#CA9EE6";
   red       = "#E78284";
-  #maroon    = "#EA999C";
+  maroon    = "#EA999C";
   peach     = "#EF9F76";
   yellow    = "#E5C890";
   green     = "#A6D189";
   teal      = "#81C8BE";
   sky       = "#99D1DB";
-  #sapphire  = "#85C1DC";
+  sapphire  = "#85C1DC";
   blue      = "#8CAAEE";
-  #lavender  = "#BABBF1";
+  lavender  = "#BABBF1";
   text      = "#C6D0F5";
   subtext1  = "#B5BFE2";
   subtext0  = "#A5ADCE";
   overlay2  = "#949CBB";
-  #overlay1  = "#838BA7";
+  overlay1  = "#838BA7";
   overlay0  = "#737994";
   surface2  = "#626880";
   surface1  = "#51576D";
-  #surface0  = "#414559";
+  surface0  = "#414559";
   base      = "#303446";
   mantle    = "#292C3C";
   crust     = "#232634";
