787e794a6201bdb42f6a14d7fcf42565d9a67433
Hervy Qurrotul Ainur Rozi <hervyqa@proton.me>
2024-02-04 14:32:51 +0700

    home: qutebrowser> catppuccin: fix even & odd colors

--- home/programs/qutebrowser.nix
+++ home/programs/qutebrowser.nix
@@ -3,31 +3,34 @@
 
   # colors
   rosewater = "#F2D5CF";
-  flamingo = "#EEBEBE";
-  pink = "#F4B8E4";
-  mauve = "#CA9EE6";
-  red = "#E78284";
-  maroon = "#EA999C";
-  peach = "#EF9F76";
-  yellow = "#E5C890";
-  green = "#A6D189";
-  teal = "#81C8BE";
-  sky = "#99D1DB";
-  sapphire = "#85C1DC";
-  blue = "#8CAAEE";
-  lavender = "#BABBF1";
-  text = "#C6D0F5";
-  subtext1 = "#B5BFE2";
-  subtext0 = "#A5ADCE";
-  overlay2 = "#949CBB";
-  overlay1 = "#838BA7";
-  overlay0 = "#737994";
-  surface2 = "#626880";
-  surface1 = "#51576D";
-  surface0 = "#414559";
-  base = "#303446";
-  mantle = "#292C3C";
-  crust = "#232634";
+  flamingo  = "#EEBEBE";
+  pink      = "#F4B8E4";
+  mauve     = "#CA9EE6";
+  red       = "#E78284";
+  maroon    = "#EA999C";
+  peach     = "#EF9F76";
+  yellow    = "#E5C890";
+  green     = "#A6D189";
+  teal      = "#81C8BE";
+  sky       = "#99D1DB";
+  sapphire  = "#85C1DC";
+  blue      = "#8CAAEE";
+  lavender  = "#BABBF1";
+  text      = "#C6D0F5";
+  subtext1  = "#B5BFE2";
+  subtext0  = "#A5ADCE";
+  overlay2  = "#949CBB";
+  overlay1  = "#838BA7";
+  overlay0  = "#737994";
+  surface2  = "#626880";
+  surface1  = "#51576D";
+  surface0  = "#414559";
+  base      = "#303446";
+  mantle    = "#292C3C";
+  crust     = "#232634";
+
+  # terang #303446
+  # gelap #393439
 
 in {
   home-manager = {
@@ -42,6 +45,7 @@ in {
             colors = {
               webpage = {
                 preferred_color_scheme = "dark";
+                bg = base;
                 darkmode = {
                   enabled = true;
                   policy = {
@@ -74,6 +78,12 @@ in {
                 scrollbar = {
                   bg = crust;
                   fg = surface2;
+                };
+                odd = {
+                  bg = mantle;
+                };
+                even = {
+                  bg = crust;
                 };
               };
               downloads= {
