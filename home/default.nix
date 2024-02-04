f25370c7b61a9836b554e657f648b52660d7de1b
Hervy Qurrotul Ainur Rozi <hervyqa@proton.me>
2024-02-04 13:09:08 +0700

    home: rstudio user libs session variables

--- home/default.nix
+++ home/default.nix
@@ -30,7 +30,10 @@ in {
           QT_QPA_PLATFORM="wayland";
           QT_WAYLAND_DISABLE_WINDOWDECORATION="1";
           SDL_VIDEODRIVER = "wayland";
+
+          # apps stuff.
           _JAVA_AWT_WM_NONREPARENTING=1;
+          R_LIBS_USER = "/home/${name}/rlibrary";
         };
       };
     };
