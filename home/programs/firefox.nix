fa1dcb1b4571bf50825e8664211ba0f18f44253c
Hervy Qurrotul Ainur Rozi <hervyqa@proton.me>
2024-02-07 03:50:47 +0700

    home: firefox> disable pkgs

--- home/programs/firefox.nix
+++ home/programs/firefox.nix
@@ -5,7 +5,7 @@ in {
     users.${name} = {
       programs = {
         firefox = {
-          enable = true;
+          enable = false;
           profiles = {
             ${name} = {
               isDefault = true;
