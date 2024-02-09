769ac9908f2b9f9f9a06f9728f4c30673f4a9ebe
Hervy Qurrotul Ainur Rozi <hervyqa@proton.me>
2024-02-09 08:04:01 +0700

    home: nix-index> disable

--- home/programs/nix-index.nix
+++ home/programs/nix-index.nix
@@ -5,7 +5,7 @@ in {
     users.${name} = {
       programs = {
         nix-index = {
-          enable = true;
+          enable = false;
           enableFishIntegration = true;
         };
       };
