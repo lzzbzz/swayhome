6b0d7c23d9abae88e69df086343918480c5f0f0c
Hervy Qurrotul Ainur Rozi <hervyqa@proton.me>
2024-02-09 08:01:30 +0700

    home: obs> disable

--- home/programs/obs.nix
+++ home/programs/obs.nix
@@ -8,7 +8,7 @@ in {
     users.${name} = {
       programs = {
         obs-studio = {
-          enable = true;
+          enable = false;
           plugins = with pkgs.obs-studio-plugins; [
             wlrobs
             obs-pipewire-audio-capture
