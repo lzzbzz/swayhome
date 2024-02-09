2bd9e90e03b99d442bb3e506b09bbd6a32006611
Hervy Qurrotul Ainur Rozi <hervyqa@proton.me>
2024-02-09 08:03:49 +0700

    home: neovim> disable

--- home/programs/neovim.nix
+++ home/programs/neovim.nix
@@ -8,7 +8,7 @@ in {
     users.${name} = {
       programs = {
         neovim = {
-          enable = true;
+          enable = false;
           viAlias = true;
           vimAlias = true;
           vimdiffAlias = true;
