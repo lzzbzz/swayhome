d1f0b8f8dfa7d45588ac5e100156a603bdc422b6
Hervy Qurrotul Ainur Rozi <hervyqa@proton.me>
2024-02-23 08:12:05 +0700

    home: zellij> sort order

--- home/programs/zellij.nix
+++ home/programs/zellij.nix
@@ -13,18 +13,18 @@ in {
             default_shell = "fish";
             pane_frames = false;
             simplified_ui = true;
-            ui = {
-              pane_frames = {
-                hide_session_name = true;
-                rounded_corners = true;
-              };
-            };
             theme = "catppuccin-frappe";
             plugins = {
               tab-bar.path = "tab-bar";
               status-bar.path = "status-bar";
               strider.path = "strider";
               compact-bar.path = "compact-bar";
+            };
+            ui = {
+              pane_frames = {
+                hide_session_name = true;
+                rounded_corners = true;
+              };
             };
           };
         };
